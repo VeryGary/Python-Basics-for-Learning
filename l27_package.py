b3998e3fd6f55d49a9dd70f2c83f06673f75ee77
VeryGary <garrettlfournier@hotmail.com>
2024-04-09 15:37:14 -0700

    Added to 27 and Pycharm stuff

---
+++ l27_package.py
@@ -0,0 +1,7 @@
+# package is a folder containing multiple modules
+# import packagename.module
+import l27_testpackage.shopping_cart
+
+# show where in memory stored
+print(l27_testpackage.shopping_cart)
+print(l27_testpackage.shopping_cart.buy('apple'))
---
+++ l28_more_Import_Ways.py
@@ -0,0 +1,7 @@
+# To import just buy
+
+# from l27_testpackage.shopping_cart import buy
+# print(buy('apple'))
+
+# To import the whole modules
+pass
